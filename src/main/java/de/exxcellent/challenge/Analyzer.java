e366b811e9e181f3d056a2d3b91b4a7d09e098b9
MeMaik <maik.vogt@pulseshift.com>
2018-01-20 15:43:40 +0100

    Updated main method and moved code to Analyzer

--- src/main/java/de/exxcellent/challenge/Analyzer.java
+++ src/main/java/de/exxcellent/challenge/Analyzer.java
@@ -1,16 +1,63 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.models.FootballData;
 import de.exxcellent.challenge.models.HasSpreadedValues;
+import de.exxcellent.challenge.models.WeatherData;
+import org.apache.commons.csv.CSVRecord;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Analyzer {
     public Analyzer() {
     }
 
+    public String findSmallestSpreadedDay(String fileName) {
+
+        CSVImporter csvImporter = new CSVImporter();
+        try {
+            ArrayList<CSVRecord> weatherRecords = csvImporter.importCSV(fileName);
+
+            ArrayList<HasSpreadedValues> weatherData = new ArrayList<HasSpreadedValues>();
+            for (CSVRecord record : weatherRecords) {
+                int day = Integer.parseInt(record.get(WeatherData.DAY_PROPERTY_NAME));
+                int maxTmp = Integer.parseInt(record.get(WeatherData.MAX_TMP_PROPERTY_NAME));
+                int minTmp = Integer.parseInt(record.get(WeatherData.MIN_TMP_PROPERTY_NAME));
+                WeatherData newWeatherData = new WeatherData(day, maxTmp, minTmp);
+                weatherData.add(newWeatherData);
+            }
+            WeatherData smallestSpreadWeatherRecord = (WeatherData) findSmallestSpreadRecord(weatherData);
+            return Integer.toString(smallestSpreadWeatherRecord.getDay());
+        } catch (IOException ioe) {
+            return "Someday";
+        }
+    }
+
+    public String findSmallestSpreadTeam(String fileName) {
+
+        CSVImporter csvImporter = new CSVImporter();
+        try {
+            ArrayList<CSVRecord> footballRecords = csvImporter.importCSV(fileName);
+            ArrayList<HasSpreadedValues> footballData = new ArrayList<HasSpreadedValues>();
+            for (CSVRecord record : footballRecords) {
+                String teamName = record.get(FootballData.TEAM_PROPERTY_NAME);
+                int goals = Integer.parseInt(record.get(FootballData.GOALS_PROPERTY_NAME));
+                int goalsAllowed = Integer.parseInt(record.get(FootballData.GOALS_ALLOWED_PROPERTY_NAME));
+                FootballData newFootballData = new FootballData(teamName, goals, goalsAllowed);
+                footballData.add(newFootballData);
+            }
+            FootballData smallestSpreadFootballRecord = (FootballData) findSmallestSpreadRecord(footballData);
+            return smallestSpreadFootballRecord.getTeamName();
+
+
+        } catch (IOException ioe) {
+            return "Some Balanced Team";
+        }
+    }
+
     //Only finds returns the first Entry with the highest Spread. A later record with the same Spread is ignored.
     //Errors with empty Lists yet.
-    public HasSpreadedValues findSmallestSpreadRecord(ArrayList<HasSpreadedValues> records) {
+    private HasSpreadedValues findSmallestSpreadRecord(ArrayList<HasSpreadedValues> records) {
         HasSpreadedValues recordWithSmallestSpread = records.stream().findFirst().get();
         int lowestSpread = recordWithSmallestSpread.calculateSpread();
         for (HasSpreadedValues record : records) {
--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,13 +1,5 @@
 package de.exxcellent.challenge;
 
-import de.exxcellent.challenge.models.FootballData;
-import de.exxcellent.challenge.models.HasSpreadedValues;
-import de.exxcellent.challenge.models.WeatherData;
-import org.apache.commons.csv.CSVRecord;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -19,45 +11,11 @@ public final class App {
     public static void main(String... args) {
 
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        String teamWithSmallesGoalSpread = "A good team"; // Your goal analysis function call …
-
-        CSVImporter csvImporter = new CSVImporter();
         String fileNameWeather = "weather.csv";
         String fileNameFootball = "football.csv";
-        try {
-            ArrayList<CSVRecord> weatherRecords = csvImporter.importCSV(fileNameWeather);
-            ArrayList<HasSpreadedValues> weatherData = new ArrayList<HasSpreadedValues>();
-            for (CSVRecord record : weatherRecords) {
-                int day = Integer.parseInt(record.get(WeatherData.DAY_PROPERTY_NAME));
-                int maxTmp = Integer.parseInt(record.get(WeatherData.MAX_TMP_PROPERTY_NAME));
-                int minTmp = Integer.parseInt(record.get(WeatherData.MIN_TMP_PROPERTY_NAME));
-                WeatherData newWeatherData = new WeatherData(day, maxTmp, minTmp);
-                weatherData.add(newWeatherData);
-            }
-            Analyzer analyzer = new Analyzer();
-            WeatherData smallestSpreadWeatherRecord = (WeatherData) analyzer.findSmallestSpreadRecord(weatherData);
-            dayWithSmallestTempSpread = Integer.toString(smallestSpreadWeatherRecord.getDay());
-
-
-            ArrayList<CSVRecord> footballRecords = csvImporter.importCSV(fileNameFootball);
-            ArrayList<HasSpreadedValues> footballData = new ArrayList<HasSpreadedValues>();
-            for (CSVRecord record : footballRecords) {
-                String teamName = record.get(FootballData.TEAM_PROPERTY_NAME);
-                int goals = Integer.parseInt(record.get(FootballData.GOALS_PROPERTY_NAME));
-                int goalsAllowed = Integer.parseInt(record.get(FootballData.GOALS_ALLOWED_PROPERTY_NAME));
-                FootballData newFootballData = new FootballData(teamName, goals, goalsAllowed);
-                footballData.add(newFootballData);
-            }
-            FootballData smallestSpreadFootballRecord = (FootballData) analyzer.findSmallestSpreadRecord(footballData);
-            teamWithSmallesGoalSpread = smallestSpreadFootballRecord.getTeamName();
-
-
-        } catch (IOException ioe) {
-            System.out.printf("Can not find %s%n", fileNameWeather);
-        }
-
-        // Your preparation code …
+        Analyzer analyzer = new Analyzer();
+        String dayWithSmallestTempSpread = analyzer.findSmallestSpreadedDay(fileNameWeather);
+        String teamWithSmallesGoalSpread = analyzer.findSmallestSpreadTeam(fileNameFootball);
 
 
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
